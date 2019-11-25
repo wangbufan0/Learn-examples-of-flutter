ae9447def1d8ed710e9dd8cae04f3bc1c7ab1565
wangbufan0 <wangbufan0@163.com>
2019-11-25 23:47:53 +0800

    add week2 for widght,day1

---
+++ Week for Widght/day1/text_route.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+class TextRoute extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Text'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text('Hello world',
+              textAlign: TextAlign.left,
+            ),
+            Text('Hello World I\'m a boy. '*4,
+              //maxLines: 1,
+              // overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            ),
+            Text('Hello world',
+              textScaleFactor: 1.5,
+            ),
+            Text('Hello world',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 18.0,
+                height: 1.2,
+                fontFamily: 'Courier',
+                background: Paint()..color=Colors.yellow,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.dashed
+              ),
+            ),
+            Text.rich(TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Home:'
+                ),
+                TextSpan(
+                  text: 'http://wangbufan.cn',
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                  recognizer: TapGestureRecognizer()..onTap=(){
+                    Toast.show('onpress', context,);
+                  },
+                )
+              ],
+            )),
+
+            DefaultTextStyle(
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('hello world'),
+                  Text('I\'m wangbufan'),
+                  Text('I\'m 20s',
+                    style: TextStyle(
+                      inherit: false,
+                      color: Colors.green
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+          ],
+      ),
+    );
+  }
+}
